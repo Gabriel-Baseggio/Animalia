3b21307afda8c7570d16e87d52c975dcd5d33d93
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2023-06-23 19:10:58 -0300

    asfsdf

--- assets/js/jogo.js
+++ assets/js/jogo.js
@@ -44,58 +44,61 @@ function init() {
             });
         } else {
             let num = parseInt(targets[i].classList[1].charAt(targets[i].classList[1].length - 1))
-            switch (num) {
-                case 0:
-                    targets[i].addEventListener('targetFound', () => {
-                        acertou(0)
-                    });
-                    break;
-                case 1:
-                    targets[i].addEventListener('targetFound', () => {
-                        acertou(1)
-                    });
-                    break;
-                case 2:
-                    targets[i].addEventListener('targetFound', () => {
-                        acertou(2)
-                    });
-                    break;
-                case 3:
-                    targets[i].addEventListener('targetFound', () => {
-                        acertou(3)
-                    });
-                    break;
-                case 4:
-                    targets[i].addEventListener('targetFound', () => {
-                        acertou(4)
-                    });
-                    break;
-                case 5:
-                    targets[i].addEventListener('targetFound', () => {
-                        acertou(5)
-                    });
-                    break;
-                case 6:
-                    targets[i].addEventListener('targetFound', () => {
-                        acertou(6)
-                    });
-                    break;
-                case 7:
-                    targets[i].addEventListener('targetFound', () => {
-                        acertou(7)
-                    });
-                    break;
-                case 8:
-                    targets[i].addEventListener('targetFound', () => {
-                        acertou(8)
-                    });
-                    break;
-                case 9:
-                    targets[i].addEventListener('targetFound', () => {
-                        acertou(9)
-                    });
-                    break;
-            }
+            targets[i].addEventListener('targetFound', () => {
+                acertou(num);
+            })
+            // switch (num) {
+            //     case 0:
+            //         targets[i].addEventListener('targetFound', () => {
+            //             acertou(0)
+            //         });
+            //         break;
+            //     case 1:
+            //         targets[i].addEventListener('targetFound', () => {
+            //             acertou(1)
+            //         });
+            //         break;
+            //     case 2:
+            //         targets[i].addEventListener('targetFound', () => {
+            //             acertou(2)
+            //         });
+            //         break;
+            //     case 3:
+            //         targets[i].addEventListener('targetFound', () => {
+            //             acertou(3)
+            //         });
+            //         break;
+            //     case 4:
+            //         targets[i].addEventListener('targetFound', () => {
+            //             acertou(4)
+            //         });
+            //         break;
+            //     case 5:
+            //         targets[i].addEventListener('targetFound', () => {
+            //             acertou(5)
+            //         });
+            //         break;
+            //     case 6:
+            //         targets[i].addEventListener('targetFound', () => {
+            //             acertou(6)
+            //         });
+            //         break;
+            //     case 7:
+            //         targets[i].addEventListener('targetFound', () => {
+            //             acertou(7)
+            //         });
+            //         break;
+            //     case 8:
+            //         targets[i].addEventListener('targetFound', () => {
+            //             acertou(8)
+            //         });
+            //         break;
+            //     case 9:
+            //         targets[i].addEventListener('targetFound', () => {
+            //             acertou(9)
+            //         });
+            //         break;
+            //}
         }
     }
 
